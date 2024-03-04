9152333b1a1b19bdfc46ade2572b146029ff1558
blue-knight99 <140939340+blue-knight99@users.noreply.github.com>
2024-03-04 12:56:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-$("h1").click(function() {
+$("#start").click(function() {
     if (!started) {
       $("#level-title").text("Level " + level);
       nextSequence();
